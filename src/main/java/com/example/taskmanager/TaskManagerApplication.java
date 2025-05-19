af5f81d9f1016f7027df80f39957b856fec2263d
bouzir <bouzirredouane10@gmail.com>
2025-05-19 17:39:59 +0100

    13

--- src/main/java/com/example/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskManagerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskManagerApplication {
+    //pour le execution a la fin
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
     }
